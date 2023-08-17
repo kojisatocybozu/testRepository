398b236a8d43c0d9db8b60daa9849c01ffd8af8d
EdwardPiano <yuche-liao@cybozu.co.jp>
2023-08-17 13:50:39 +0800

    test.js edited

--- edo/test.js
+++ edo/test.js
@@ -1,5 +1,6 @@
-const message_str = "test 12345";
-
-console.log(message_str);
-console.log("test 12345");
-console.log("test 54321");
+const message_str1 = "message_str1";
+const message_str2 = "message_str2";
+const message_str3 = "message_str3";
+console.log(message_str1);
+console.log(message_str2);
+console.log(message_str3);
